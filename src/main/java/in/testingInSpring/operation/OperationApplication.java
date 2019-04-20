f727d34450489d36a3f5494e326f649d8b02f5c1
Vidhan <vidhanvats@yahoo.com>
2019-04-20 15:40:29 +0530

    change code to make jar to war.

--- src/main/java/in/testingInSpring/operation/OperationApplication.java
+++ src/main/java/in/testingInSpring/operation/OperationApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OperationApplication {
+public class OperationApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(SpringBootWebSecurityConfiguration.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OperationApplication.class, args);
