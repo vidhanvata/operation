6fa9579d01ce3757e837620e7c6d865dae4f6dac
Vidhan <vidhanvats@yahoo.com>
2019-04-20 16:12:42 +0530

    change in main file.

--- src/main/java/in/testingInSpring/operation/OperationApplication.java
+++ src/main/java/in/testingInSpring/operation/OperationApplication.java
@@ -12,11 +12,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class OperationApplication extends SpringBootServletInitializer{
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-		return applicationBuilder.sources(SpringBootWebSecurityConfiguration.class);
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(OperationApplication.class, args);
 	}
